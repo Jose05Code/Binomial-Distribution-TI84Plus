8ee0bf67714fa6d922aee78229d06e3b37f98032
Jose Frade Gala <152511300+Jose05Code@users.noreply.github.com>
2025-10-15 12:00:15 -0400

    fix(binomial_distribution): func() do not need an argument

---
+++ binomial_distribution.py
@@ -0,0 +1,19 @@
+import bino_library_ui as ui
+
+def main():
+    while True:
+        ui.menu()
+        choice = ui.get_user_choice(5)
+        switcher = {
+            1: ui.probability_greater_than,
+            2: ui.probability_greater_equal,
+            3: ui.probability_less_than,
+            4: ui.probability_less_equal,
+            5: ui.probability_equal
+        }
+        func = switcher.get(choice)
+        func()
+
+
+if __name__ == "__main__":
+    main()
