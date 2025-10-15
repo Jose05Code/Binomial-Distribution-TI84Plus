ecf614bc49f63f7f68acdc70ba3fa1b3740d5f65
Jose Frade Gala <152511300+Jose05Code@users.noreply.github.com>
2025-10-15 11:23:22 -0400

    feat(bino_library_ui): add interactive main  menu

---
+++ bino_library_ui.py
@@ -0,0 +1,9 @@
+def menu():
+    print("===============================")
+    print("Welcome to the Bino Library")
+    print("1. P(x > a)")
+    print("2. P(x >= a)")
+    print("3. P(x < a)")
+    print("4. P(x <= a)")
+    print("5. P(x = a)")
+    print("===============================")
