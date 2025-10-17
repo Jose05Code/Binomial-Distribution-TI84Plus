528f02eed530d059f30137d67727446a7198fd30
Jose Frade Gala <152511300+Jose05Code@users.noreply.github.com>
2025-10-17 12:18:55 -0400

    feat(test): Adds test for binomial density function
    Adds a test case for the binomial probability mass
    function to ensure correct calculation of binomial
    probabilities.

--- src/LIB_MATH.py
+++ src/LIB_MATH.py
@@ -6,4 +6,5 @@ def nCr(n, a):
     
     return nCr(n-1, a-1) + nCr(n-1, a)
 
-
+def binopdf(n, p, a):
+    return nCr(n, a)*(p**a)*(1-p)**(n-a)
--- test/test_math.py
+++ test/test_math.py
@@ -4,3 +4,9 @@ def test_nCr():
     n = 20
     r = 4
     assert math.nCr(n, r) == 4845
+
+def test_binopdf():
+    n = 10
+    p = 0.5
+    a = 6
+    assert math.binopdf(n, p, a) == 0.205078125
