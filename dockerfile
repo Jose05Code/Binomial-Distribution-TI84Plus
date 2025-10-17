f096c8470f5a896c719097272e9b72703471671b
Jose Frade Gala <152511300+Jose05Code@users.noreply.github.com>
2025-10-17 02:31:38 -0400

    fix(tests): allow importing src from tests

--- dockerfile
+++ dockerfile
@@ -1,10 +1,17 @@
 FROM python:3.13.9-slim
 
+RUN mkdir /app/
+COPY requirements.txt /app/
+COPY ./setup.py /app/
+
+COPY src/ /app/src/
+
 RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r /app/requirements.txt
+RUN pip install -e /app
 
 # pytest requirements
 WORKDIR /app/
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-CMD [ "pytest" ]
 
+CMD [ "pytest" ]
+ENV PYTHONDONTWRITEBYTECODE=true
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+long_description = """
+Binomial Distribution TI-84 Plus - package configuration for local editable installs
+"""
+
+version = "1.0.0"
+
+requirements = [
+]
+
+if __name__ == '__main__':
+    setup(
+        name='binomial_distribution_ti84plus',
+        version=version,
+        description='Binomial utils and tests for TI-84 project',
+        long_description=long_description,
+        author="Jose",
+        packages=find_packages(
+            exclude=[
+                'tests',
+            ],
+            include=[
+                'src',
+                'utils',
+            ],
+        ),
+    license='MIT',
+    install_requires=requirements,
+            classifiers=[
+          'Development Status :: 1 - Planning',
+          'Framework :: Pytest',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+    )
